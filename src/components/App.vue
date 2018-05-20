f1b30e7b8fb730cc22a42540c352de2797984857
marcpre <marcus.presich@gmail.com>
2018-05-20 06:48:37 +0000

    adding days

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>{{ msg }}</div>
+    <div>
+        <div v-for="day in days">{{ day }}</div>
+    </div>
 </template>
+
 <script>
     export default {
         data() {
             return {
-                msg: 'Hello World'
-            };
+                month: 2,
+                year: 2018,
+            }
+        },
+        computed: {
+            days() {
+                let days = []
+                let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D')
+                do {
+                    days.push(currentDay)
+                    currentDay = this.$moment(currentDay).add(1, 'days')                    
+                } while(currentDay.month() + 1 === this.month)
+                return days
+            }
         }
     }
 </script>
