8d9ffa8baf1be844e2072f4ab63bdf28dfdbff67
marcpre <marcus.presich@gmail.com>
2018-05-23 04:17:32 +0000

    grouping days in weeks

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <div v-for="day in days">{{ day }}</div>
+        <div v-for="week in weeks">
+            Week
+            <div v-for="day in weeks">{{ day }}</div>
+        </div>
     </div>
 </template>
 <script>
@@ -46,6 +49,19 @@
                 }
 
                 return days;
+            },
+            weeks() {
+                let weeks = []
+                let week = []
+                
+                for(let day of this.days) {
+                    week.push(day)
+                    if(week.length === 7) {
+                        weeks.push(week)
+                        week = []
+                    }
+                }
+                return weeks
             }
         }
     }
