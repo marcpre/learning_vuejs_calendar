c59a9537b609d22d1322f99773053ed1e0687410
marcpre <marcus.presich@gmail.com>
2018-05-28 04:20:48 +0000

    capture event form click

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,6 +1,16 @@
 <template>
-    <div id="event-form" :class="{ active: true }" :style="{ top: '500px', left: '500px' }"></div>
+    <div id="event-form" :class="{ active: true }" :style="{ top: top, left: left }"></div>
 </template>
 <script>
+export default {
+    computed: {
+        top() {
+            return `${this.$store.state.eventFormPosY}px`          
+        }, 
+        left() {
+            return `${this.$store.state.eventFormPosX}px`
+        }
+    }
+}
     
 </script>
