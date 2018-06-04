c2213bf3593210b5aefd789d008ed7dec6e7a6c3
marcpre <marcus.presich@gmail.com>
2018-06-04 04:32:41 +0000

    move initial state data to html

--- src/web.entry.js
+++ src/web.entry.js
@@ -9,12 +9,19 @@ Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.mome
 
 import App from './components/App.vue';
 
-let events = [
-    { description: 'Random event 1', date: moment('2018-06-06', 'YYYY-MM-DD') },
-    { description: 'Random event 2', date: moment('2018-06-15', 'YYYY-MM-DD') },
-    { description: 'Random event 3', date: moment('2018-06-21', 'YYYY-MM-DD') }
-  ]
-let initialState = Object.assign({}, store.state, {events})
+// let events = [
+//    { description: 'Random event 1', date: moment('2018-06-06', 'YYYY-MM-DD') },
+//    { description: 'Random event 2', date: moment('2018-06-15', 'YYYY-MM-DD') },
+//    { description: 'Random event 3', date: moment('2018-06-21', 'YYYY-MM-DD') }
+//  ]
+let events = window.__INITIAL_STATE__.map(event => {
+  return {
+    description: event.description,
+    date: moment(event.date)
+  }
+})
+
+let initialState = Object.assign({}, store.state, { events: events })
 store.replaceState(initialState)
 
 new Vue({
